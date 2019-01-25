604bf1c134861813d6825b65dd64bb46c9801ff5
NeAlexei <a.nechitaylo@hypervsn.com>
2019-01-25 15:57:43 +0300

    TestFromSergei

--- src/main/java/com/kinomo/streaming/test/TestRunner.java
+++ src/main/java/com/kinomo/streaming/test/TestRunner.java
@@ -40,33 +40,23 @@ public class TestRunner {
     private static void runStressTest() throws IOException, InterruptedException {
         byte[] fileContent = Files.readAllBytes(Paths.get(FILE_PATH));
 
-        /*for (int n = 5; n < 20; n+=3){
-            logger.info("============= Number of concurrent clients: " + n + " ===================");
-
-            for (int k = 0; k < 5; k++) {*/
-
-                int n = 40;
-
-                final ExecutorService executorService = Executors.newFixedThreadPool(20);
+        final ExecutorService executorService = Executors.newFixedThreadPool(20);
 
 
-                for (int i = 0; i < n; i++) {
-                    executorService.submit(new UploadDownloadTask(fileContent));
-                    Thread.sleep(2 * 1000);
-                }
+        for (int i = 0; i < 40; i++) {
+            executorService.submit(new UploadDownloadTask(fileContent));
+            Thread.sleep(2 * 1000);
+        }
 
-                executorService.shutdown();
-              //  executorService.awaitTermination(10, TimeUnit.MINUTES);
-                Metrics.print();
-                Metrics.reset();
-                logger.info("\n\n");
-             //   Thread.sleep(1 * 60 * 1000);
-          /*     }
-
-         FileFinder.deleteDownloadedFiles();
-            Thread.sleep(3 * 60 * 1000); */
-
+        executorService.shutdown();
+        Metrics.print();
+        Metrics.reset();
+        logger.info("\n\n");
     }
+
+
+
+
 
     private static void runImagesStressTest() throws InterruptedException {
 
--- src/main/java/com/kinomo/streaming/test/config/Settings.java
+++ src/main/java/com/kinomo/streaming/test/config/Settings.java
@@ -3,21 +3,21 @@ package com.kinomo.streaming.test.config;
 public class Settings {
 
     //public static final String FILE_PATH = "D:\\_code\\videotest\\jellyfish-25-mbps-hd-hevc.mp4";
-    public static final String FILE_PATH = "D:\\_code\\streaming-test\\burger_880x880.mp4";
+    public static final String FILE_PATH = "D:\\Java\\streaming-test\\src\\main\\resources\\vaselin.mp4";
 
-   private static final String BASE_URL = "http://localhost:8089";
+   private static final String BASE_URL = "https://test02-platform.kino-mo.com";
     //private static final String BASE_URL = "http://test01-http-platform.kino-mo.com/api/v1";
    // private static final String BASE_URL = "https://dev-platform.kino-mo.com/api/v1";
 
     //public static final String DOWNLOAD_DATA_URL = BASE_URL + "/cct-content/images/:remoteFileId/data";
-    public static final String DOWNLOAD_DATA_URL = BASE_URL + "/asset-library/:remoteFileId/asset.sketchfabmodel";
+    public static final String DOWNLOAD_DATA_URL = BASE_URL + "/api/v1/cct-content/videos/:remoteFileId/data";
     //public static final String DOWNLOAD_DATA_URL = BASE_URL + "/cct-content/staticimage";
-    public static final String UPLOAD_URL =  BASE_URL + "/cct-content/videos";
-    public static final String METADATA_URL = BASE_URL + "/cct-content/videos/:remoteFileId";
+    public static final String UPLOAD_URL =  BASE_URL + "/api/v1/cct-content/videos";
+    public static final String METADATA_URL = BASE_URL + "/api/v1/cct-content/videos/:remoteFileId";
 
     public static final String KM_AUTH = "km-auth";
-  //  public static final String SESSION_TOKEN = "45f4d842b8b9d7f1dad1167ba29ee6d730c5722024e04a620a98cde3adf4e4295bc9ef9cea878b378c7031a5";
-    public static final String SESSION_TOKEN = "123";
+    public static final String SESSION_TOKEN = "1f91af6826f434cc1f178b272662d656c47698d566c7b29150fcb9bb01db7bd05beed78ef2331e1503716bd3";
+//    public static final String SESSION_TOKEN = "123";
 
-    public static final String DOWNLOADED_FILE_EXTENSION = "mp4";
+    public static final String DOWNLOADED_FILE_EXTENSION = ".mp4";
 }
